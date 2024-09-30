6979324dd55be4cf94fe9af39b40d1158824de8d
Divyanshu Prasad <divyanshu9871@gmail.com>
2024-09-30 15:35:15 +0530

    added assitant using langchain and prompt templates

--- main.py
+++ main.py
@@ -1,19 +1,21 @@
 import os
-import google.generativeai as genai
+from langchain.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain.schema.messages import SystemMessage
+from langchain_community.chat_message_histories import ChatMessageHistory
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables.history import RunnableWithMessageHistory
+from langchain_google_genai import ChatGoogleGenerativeAI
 from mem0 import Memory
 from dotenv import load_dotenv
 
 load_dotenv()
 
 user_id = "divyanshu"
-os.environ["GEMINI_API_KEY"] = os.getenv("GEMINI_API_KEY")
+os.environ["GOOGLE_API_KEY"] = os.getenv("GOOGLE_API_KEY")
 
 qdrant_collection_name = os.getenv("QDRANT_COLLECTION_NAME")
 qdrant_url = os.getenv("QDRANT_URL")
 qdrant_api_key = os.getenv("QDRANT_API_KEY")
-
-
-genai.configure(api_key=os.environ["GEMINI_API_KEY"])
 
 config = {
     "llm": {
@@ -59,36 +61,11 @@ memory = Memory.from_config(config_dict=config)
 #     memory.add(mem, user_id=user_id, metadata=meta)
 
 
-class Companion:
-    """
-    A class representing an AI companion that can engage in conversations and remember previous interactions.
-    """
+class Assistant:
+    def __init__(self, llm):
+        self.chain = self._create_inference_chain(llm)
 
-    def __init__(self):
-        """
-        Initialize the Companion.
-        """
-        self.model = genai.GenerativeModel(
-            model_name="gemini-1.5-flash-002",
-            generation_config={
-                "temperature": 1,
-                "top_p": 0.95,
-                "top_k": 40,
-                "max_output_tokens": 1024,
-                "response_mime_type": "text/plain",
-            },
-            system_instruction="You are an AI personal assistant with context awareness and long-term memory. Your job is to assist the user, remember key details from conversations, and provide personalized support. Use past interactions to adapt responses and make future conversations more efficient. Respond naturally and directly, like a human, without explaining the reasoning behind your responses or why you chose them. Focus on delivering helpful, concise answers without clarifications. Continuously adapt to the user's needs.",
-        )
-        self.chat_session = None
-        self.memory = memory
-
-    def start_chat(self):
-        """
-        Start a chat session.
-        """
-        self.chat_session = self.model.start_chat()
-
-    def ask(self, question: str, user_id: str) -> str:
+    def answer(self, question: str, user_id: str) -> str:
         """
         Process a user's question and generate a response.
 
@@ -96,56 +73,71 @@ class Companion:
         :param user_id: The unique identifier for the user.
         :return: The AI-generated response to the question.
         """
-        if self.chat_session is None:
-            self.start_chat()
+        if not question:
+            return
 
-
-        previous_memories = memory.search(question, user_id=user_id, limit=5)
+        previous_memories = memory.search(question, user_id=user_id, limit=3)
 
         relevant_memories_text = "\n".join(
             mem["memory"] for mem in previous_memories["results"]
         )
 
         prompt = f"User input: {question}\nPrevious memories: {relevant_memories_text}"
-        response = self.chat_session.send_message(prompt)
+        response = self.chain.invoke(
+            {"prompt": prompt},
+            config={"configurable": {"session_id": "unused"}},
+        )
 
-        return response.text
+        return response
 
-    def __enter__(self):
+    def _create_inference_chain(self, llm):
+        SYSTEM_PROMPT = """
+        You are an AI personal assistant with context awareness and long-term memory. Your job is to assist the user, remember key details from conversations, and provide personalized support. Use past interactions to adapt responses and make future conversations more efficient. Respond naturally like a human, without explaining the reasoning behind your responses or why you chose them.
         """
-        Enter the runtime context for the Companion.
 
-        :return: The Companion instance.
-        """
-        return self
+        prompt_template = ChatPromptTemplate.from_messages(
+            [
+                SystemMessage(content=SYSTEM_PROMPT),
+                MessagesPlaceholder(variable_name="chat_history"),
+                (
+                    "human",
+                    [
+                        {"type": "text", "text": "{prompt}"},
+                    ],
+                ),
+            ]
+        )
 
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        """
-        Exit the runtime context for the Companion.
+        chain = prompt_template | llm | StrOutputParser()
 
-        :param exc_type: The exception type, if any.
-        :param exc_val: The exception value, if any.
-        :param exc_tb: The exception traceback, if any.
-        """
-        pass
+        chat_message_history = ChatMessageHistory()
+        return RunnableWithMessageHistory(
+            chain,
+            lambda _: chat_message_history,
+            input_messages_key="prompt",
+            history_messages_key="chat_history",
+        )
+
+
+llm = ChatGoogleGenerativeAI(
+    model="gemini-1.5-flash-002",
+    temperature=0.2,
+    max_tokens=1024,
+)
 
 
 def main():
-    """
-    The main function to run the AI companion interaction loop.
-    """
-    with Companion() as ai_companion:
-        while True:
-            text_input = input("\nAsk me something: (or 'quit' to exit):\t")
+    assistant = Assistant(llm)
 
-            if text_input.lower() == "quit":
-                print("Goodbye :)")
-                break
-            print("\nAssistant:")
-            answer = ai_companion.ask(
-                text_input, user_id=user_id
-            ) 
-            print(answer)
+    while True:
+        user_input = input("\nAsk me something: (or 'quit' to exit):\t")
+
+        if user_input.lower() == "quit":
+            print("Goodbye :)")
+            break
+        print("\nAssistant:")
+        answer = assistant.answer(user_input, user_id=user_id)
+        print(answer)
 
 
 if __name__ == "__main__":
